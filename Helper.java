45d247091b3deac65a2c0468851200ef7764aa74
Keigen Godlaski <160975086+godlaska@users.noreply.github.com>
2024-08-27 20:04:07 -0400

    Updated Zpm.java Created Helper.java

---
+++ Helper.java
@@ -0,0 +1,6 @@
+import java.io.File;
+
+public class Helper {
+    public Helper(File file) {
+    }
+}
--- Zpm.java
+++ Zpm.java
@@ -1,2 +1,21 @@
+import java.io.File;
+
 public class Zpm {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Please input a file");
+            System.exit(0);
+        }
+        try {
+            File file = new File(args[0]);
+            // Runs compiler logic
+            Helper openFile = new Helper(file);
+        }
+        catch (Exception e) {
+            System.err.println("Cannot open file.");
+            System.exit(0);
+        }
+
+    }
 }
