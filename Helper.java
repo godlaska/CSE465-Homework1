1f4fbf31bcc80ee67521d8b19180d619d9e6e1e5
Keigen Godlaski <160975086+godlaska@users.noreply.github.com>
2024-08-30 11:41:42 -0400

    Added JavaDoc for Zpm.java Added line processing in Helper.java

--- Helper.java
+++ Helper.java
@@ -1,6 +1,52 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Helper {
+    HashMap<String, String> data;
+    File loadedFile;
+    Scanner fileReader;
+    int lineNumber;
+
     public Helper(File file) {
+        // Stores the lines in a hashmap for unordered access
+        data = new HashMap<String, String>();
+
+        // Store the file location as an instance
+        this.loadedFile = file;
+
+        try {
+            fileReader = new Scanner(loadedFile);
+        } catch (Exception e) {
+            System.out.println("Unable to load the file: " + loadedFile + ". " +
+                    "Double check to make sure this file exists.");
+        }
+
+        process();
+    }
+
+    private void process() {
+        lineNumber = 1;
+
+        while (fileReader.hasNextLine()) {
+            // Reads the data one line at a time
+            String line = fileReader.nextLine();
+
+            // Trims whitespace
+            String processedLine = line.trim();
+
+            // Store the data with line number and full string for future
+            // processing
+            data.put("Line " + lineNumber, processedLine);
+
+            lineNumber++;
+        }
+
+        fileReader.close();
+        print();
+    }
+
+    private void print() {
+        System.out.println(data.toString());
     }
 }
--- Zpm.java
+++ Zpm.java
@@ -1,6 +1,18 @@
 import java.io.File;
 
+/**
+ * The Zpm class is an entry point for the compiler. It takes a .zpm file as a
+ * command-line argument and passes it to the Helper class for the compiler
+ * processing logic. If no arguments are provided or if the file cannot be
+ * opened, the program will print an error message and exit.
+ */
 public class Zpm {
+    /**
+     * The main method of the program.
+     *
+     * @param args Command-line arguments. The first argument should be a
+     *             .zpm file to be processed
+     */
     public static void main(String[] args) {
 
         if (args.length == 0) {
